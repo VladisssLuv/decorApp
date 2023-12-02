aa4251e66921899523d272b1c750a93a0c45fb4a
VladisssLuv <vladissslav19@gmail.com>
2023-12-02 16:14:27 +0300

    add: binding and test build project)

--- app/src/main/java/com/hackathon/decor/MainActivity.kt
+++ app/src/main/java/com/hackathon/decor/MainActivity.kt
@@ -2,10 +2,12 @@ package com.hackathon.decor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hackathon.decor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
